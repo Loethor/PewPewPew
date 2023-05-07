d395f599f3a1406b49b5ac75ff5b8c4123d5a774
loethor <lrb2392@gmail.com>
2023-05-07 21:23:56 +0200

    Added basic multiplayer logic

--- world.gd
+++ world.gd
@@ -1,5 +1,38 @@
 extends Node
 
+# References
+@onready var main_menu = $CanvasLayer/MainMenu
+@onready var address_entry = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/AddressEntry
+
+const Player = preload("res://player.tscn")
+const PORT = 9999
+var enet_peer = ENetMultiplayerPeer.new()
+
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
+
+#server
+func _on_host_button_pressed():
+	main_menu.hide()
+	
+	# Creates the server
+	enet_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	multiplayer.peer_connected.connect(add_player)
+	
+	# Adds the player
+	add_player(multiplayer.get_unique_id())
+
+#client
+func _on_join_button_pressed():
+	main_menu.hide()
+	
+	# Creates the client
+	enet_peer.create_client("localhost", PORT)
+	multiplayer.multiplayer_peer = enet_peer
+
+func add_player(peer_id):
+	var player = Player.instantiate()
+	player.name = str(peer_id)
+	add_child(player)
