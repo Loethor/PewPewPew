64cc186221d65b389487b68c309817ce03c746e5
loethor <lrb2392@gmail.com>
2023-05-07 11:03:11 +0200

    Added camera controls

--- Player.gd
+++ Player.gd
@@ -1,14 +1,28 @@
 extends CharacterBody3D
 
+# References
+@onready var camera = $Camera3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+# Constants 
+const SPEED = 10.0
+const JUMP_VELOCITY = 10.0
+const CAMERA_ROTATION_SPEED = 0.005
+const MAXIMUM_CAMERA_ROTATION = PI/2
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity = 20.0
 
+# Functions
 func _ready():
+	# Captures the mouse to the middle of the screen
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		# Handling camera movement
+		rotate_y(-event.relative.x * CAMERA_ROTATION_SPEED)
+		camera.rotate_x(-event.relative.y * CAMERA_ROTATION_SPEED)
+		camera.rotation.x = clamp(camera.rotation.x, -MAXIMUM_CAMERA_ROTATION, MAXIMUM_CAMERA_ROTATION)
 
 func _physics_process(delta):
 	# Add the gravity.
